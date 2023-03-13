33aaf6a3cbb2c89f057a001c5a84488e08684df3
BCPSJames <james.vanmeter@bullitt.kyschools.us>
2023-03-13 13:58:11 -0400

    Re-created the Navbar, and it styles.

---
+++ Scripts/Utilities.js
@@ -0,0 +1,9 @@
+function navbarFunction(){
+    var x = document.getElementById("myLinks");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
