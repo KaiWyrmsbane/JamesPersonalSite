79eeca5d7ae0bde8908f28c905e9e7cff4bfa6f3
BCPSJames <james.vanmeter@bullitt.kyschools.us>
2023-03-21 18:38:29 -0400

    adding code to contact,projects, and resume pages

--- Scripts/Utilities.js
+++ Scripts/Utilities.js
@@ -7,3 +7,19 @@ function navbarFunction(){
         x.style.display = "block";
     }
 }
+
+//accordian stuff
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
